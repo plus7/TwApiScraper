5ed7cd520314e963f8b83450073f0d9f2a8ebe05
ahya <ahya@ahya-magnate.is.s.u-tokyo.ac.jp>
2010-02-10 07:03:59 +0900

    remove

--- api.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-i=0
-cat $1 | while read s; do ### 各行をsに代入。行ごとに以下を実行
-  ./makestub.sh $s
-done
--- scraper.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-rm apis/*
-lynx -dump "http://apiwiki.twitter.com/Twitter-API-Documentation" > index.txt
-grep -o "http://.*$" < index.txt > index.urls
-grep Twitter-REST-API < index.urls > restapi.list
-grep Twitter-Search-API < index.urls > searchapi.list
-./api.sh restapi.list
-./api.sh searchapi.list
